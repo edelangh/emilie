44bf11e8c1aba2d917ce179f46c0593ce0496698
edelangh <edelangh@student.42.fr>
2017-10-03 02:12:25 +0200

    ask for export GOOGLE_SPEECH_KEY

--- srcs/config/global.js
+++ srcs/config/global.js
@@ -1,3 +1,9 @@
+
+if (!process.env["GOOGLE_SPEECH_KEY"]) {
+    console.log("[config][error] pliz export your GOOGLE_SPEECH_KEY");
+    process.exit();
+}
+
 exports.google = {
     "url": "https://content-speech.googleapis.com",
     "page": "/v1/speech:recognize",
